c3d645b404df1b18a3ae43aeec6850caae318371
NqkufTam <52350586+nqkuftam@users.noreply.github.com>
2025-06-17 19:46:07 +0300

    Add files via upload

--- properties.php
+++ properties.php
@@ -313,10 +313,9 @@ try {
         
         <?php if ($currentBuilding): ?>
         <div class="building-info">
-            <h4><i class="fas fa-building"></i> Текуща сграда: <?php echo htmlspecialchars($currentBuilding['name']); ?></h4>
+            <h4><i class="fas fa-building"></i> Текуща сграда:         <?php echo renderBuildingSelector(); ?></h4>
             <p><i class="fas fa-map-marker-alt"></i> Адрес: <?php echo htmlspecialchars($currentBuilding['address']); ?></p>
         </div>
-        <?php echo renderBuildingSelector(); ?>
         <?php endif; ?>
         
         <?php if ($error): ?>
